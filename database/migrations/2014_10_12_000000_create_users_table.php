b7fcb4be4e7ff750d61c596f8106d75a556350bd
eeneg <generellanos@gmail.com>
2023-12-01 16:27:44 +0800

    removed role in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role_id')
-                ->references('id')
-                ->on('roles')
-                ->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
