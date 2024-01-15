33cd1f571dbe2350eb17099d5a90f258a49f2a0f
eeneg <generellanos@gmail.com>
2024-01-15 15:48:55 +0800

    file

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FileController extends Controller
 {
@@ -11,7 +12,7 @@ class FileController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Files/Index');
     }
 
     /**
--- resources/js/Pages/Files/Index.vue
+++ resources/js/Pages/Files/Index.vue
@@ -1,5 +1,8 @@
 <script setup>
     import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+    import TextInput from '@/Components/TextInput.vue';
+
 
 
 </script>
@@ -8,7 +11,7 @@
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">Attachments</h2>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">File Upload</h2>
         </template>
 
         <div class="py-5">
@@ -17,13 +20,65 @@
                     <div class="flex flex-row">
                         <div class="pl-5 pr-6 mt-3 grow">
                             <header>
-                                <h2 class="text-lg font-medium text-gray-900">Search Attachments</h2>
+                                <h2 class="text-lg font-medium text-gray-900">Upload File</h2>
 
                                 <p class="mt-1 text-sm text-gray-600">
-                                    File searching
+                                    Words here
                                 </p>
                             </header>
+                            <TextInput type="text" placeholder="Storage Location"/> <br>
+                            <TextInput type="text" placeholder="Details"/>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-5">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg mt-4">
+                    <div class="pl-5 pr-6 mt-3 grow">
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900">Words here</h2>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                Words here
+                            </p>
+                        </header>
+                        <table class="w-full mt-5">
+                            <thead>
+                                <tr>
+                                    <th class="border-b border-slate-300" style="width: 30%;">
+                                        Title
+                                    </th>
+                                    <th class="border-b border-slate-300" style="width: 30%;">
+                                        Date
+                                    </th>
+                                    <th class="border-b border-slate-300" style="width: 30%;">
+                                        Input Minutes
+                                    </th>
+                                    <th class="border-b border-slate-300" style="width: 10%;">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-slate-700">
+                                <tr>
+                                    <td class="p-2 text-center border-b border-slate-100">
+                                        asd
+                                    </td>
+                                    <td class="p-2 text-center border-b border-slate-100">
+                                        asd
+                                    </td>
+                                    <td class="p-2 text-center border-b border-slate-100">
+                                        asd
+                                    </td>
+                                    <td class="p-2 text-center border-b border-slate-100">
+                                        asd
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
