5fd580a8a62f06704ef4cf1798606bf924f8eaed
eeneg <generellanos@gmail.com>
2024-01-30 16:21:23 +0800

    pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -30,6 +30,7 @@
                 v-for="(link, index, key) in data.links"
                 :key="key"
                 :href="(link.url ?? '#')"
+                :preserve-scroll="true"
                 :class="{'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : link.active == true, 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0' : link.active == false }"
             >
                 <ChevronLeftIcon v-if="index == 0" class="h-5 w-5" aria-hidden="true" />
