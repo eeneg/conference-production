56f53f6098c51c9854eda3d484cb5ecb0aa1e302
eeneg <generellanos@gmail.com>
2024-02-14 16:42:23 +0800

    chat

--- resources/js/Components/ChatBox.vue
+++ resources/js/Components/ChatBox.vue
@@ -5,6 +5,108 @@
     const a = 2
 </script>
 
+<style>
+body {
+  font-family: helvetica;
+  display: flex ;
+  flex-direction: column;
+  align-items: center;
+}
+
+.chat {
+  width: 300px;
+  border: solid 1px #EEE;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+
+.messages {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+}
+
+.message {
+  border-radius: 20px;
+  padding: 8px 15px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  display: inline-block;
+}
+
+.yours {
+  align-items: flex-start;
+}
+
+.yours .message {
+  margin-right: 25%;
+  background-color: #eee;
+  position: relative;
+}
+
+.yours .message.last:before {
+  content: "";
+  position: absolute;
+  z-index: 0;
+  bottom: 0;
+  left: -7px;
+  height: 20px;
+  width: 20px;
+  background: #eee;
+  border-bottom-right-radius: 15px;
+}
+.yours .message.last:after {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: -10px;
+  width: 10px;
+  height: 20px;
+  background: white;
+  border-bottom-right-radius: 10px;
+}
+
+.mine {
+  align-items: flex-end;
+}
+
+.mine .message {
+  color: white;
+  margin-left: 25%;
+  background: linear-gradient(to bottom, #00D0EA 0%, #0085D1 100%);
+  background-attachment: fixed;
+  position: relative;
+}
+
+.mine .message.last:before {
+  content: "";
+  position: absolute;
+  z-index: 0;
+  bottom: 0;
+  right: -8px;
+  height: 20px;
+  width: 20px;
+  background: linear-gradient(to bottom, #00D0EA 0%, #0085D1 100%);
+  background-attachment: fixed;
+  border-bottom-left-radius: 15px;
+}
+
+.mine .message.last:after {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  right: -10px;
+  width: 10px;
+  height: 20px;
+  background: white;
+  border-bottom-left-radius: 10px;
+}
+
+</style>
+
 <template>
     <div>
         <div class="mx-auto w-96">
@@ -25,34 +127,33 @@
                 </div>
 
                 <div class="pl-6 pr-6 mt-3 h-80 overflow-auto">
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
-                    asd <br>
+                    <div class="w-full">
+                        <div class="mine messages">
+                            <div class="message last">
+                            Dude
+                            </div>
+                        </div>
+                        <div class="yours messages">
+                            <div class="message">
+                            Hey!
+                            </div>
+                            <div class="message">
+                            You there?
+                            </div>
+                            <div class="message last">
+                            Hello, how's it going?
+                            </div>
+                        </div>
+                        <div class="mine messages">
+                            <div class="message">
+                            Great thanks!
+                            </div>
+                            <div class="message last">
+                                How about you? How about you? How about you? How about you?
+                                How about you? How about you? How about you?How about you?
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="flex p-2">
