6c8a8535b6b539d135d04bc33ec5082917d64693
eeneg <generellanos@gmail.com>
2024-02-29 16:51:05 +0800

    storage

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\File;
+use App\Modesl\Attachment;
 
 class Storage extends Model
 {
     use HasFactory, HasUuids;
 
     protected $fillable = ['storage_id', 'title', 'location', 'details'];
+
+    public function attachment(){
+        return $this->hasMany(Attachment::class);
+    }
 
     public function files(){
         return $this->hasMany(File::class);
