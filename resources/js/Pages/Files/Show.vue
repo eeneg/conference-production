fac03a454f50d258dcf138edb32253ce3782888e
eeneg <generellanos@gmail.com>
2024-02-29 16:51:17 +0800

    file

---
+++ resources/js/Pages/Files/Show.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+    import FindFileLayout from '@/Layouts/FindFileLayout.vue';
+
+
+    const props = defineProps({files: Object})
+
+
+</script>
+<template>
+    <Head title="Attachments" />
+
+    <FindFileLayout :type="2">
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">Attachments</h2>
+        </template>
+    </FindFileLayout>
+
+</template>
