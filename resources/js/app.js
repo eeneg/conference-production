55098f93d633a22b4ffa74fd243118e258992275
eeneg <generellanos@gmail.com>
2024-03-19 16:54:22 +0800

    views

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,20 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import AuthenticatedLayout from './Layouts/AuthenticatedLayout.vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/index.js';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout
+        || AuthenticatedLayout
+        return page
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
