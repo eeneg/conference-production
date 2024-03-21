6c63bb60239b255c4b8024d45f3dfee317f2ef19
eeneg <generellanos@gmail.com>
2024-03-21 16:31:39 +0800

    poll

---
+++ app/Models/Poll.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['title', 'agree_count', 'disagree_count', 'undecided_count', 'result', 'details'];
+
+    public function conference() : BelongsTo{
+        return $this->belongsTo(Conference::class);
+    }
+}
---
+++ database/migrations/2024_03_21_081458_create_polls_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->uuid('conference_id');
+            $table->foreign('conference_id')
+                ->references('id')
+                ->on('conferences');
+            $table->string('title');
+            $table->integer('agree_count');
+            $table->integer('disagree_count');
+            $table->integer('undecided_count');
+            $table->string('result');
+            $table->string('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('polls');
+    }
+};
