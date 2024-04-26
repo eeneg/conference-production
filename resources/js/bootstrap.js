c83d2b189abd91a4324d2555afacdaeb987f7584
eeneg <generellanos@gmail.com>
2024-04-26 19:52:46 +0800

    ws

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,7 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    // broadcaster: 'pusher',
     // key: import.meta.env.VITE_PUSHER_APP_KEY,
     // cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     // wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
